007b9150bfd9e024695fa91862599e5c1e1fe386
Юрий <90134356+yuras11@users.noreply.github.com>
2022-05-07 20:17:38 +0300

    Update flappybird.h

--- flappybird.h
+++ flappybird.h
@@ -11,8 +11,12 @@
 #include <QTimer>
 #include <QKeyEvent>
 #include <vector>
+#include <string>
 #include <random>
 #include <QImage>
+#include <QString>
+#include <QFont>
+
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class FlappyBird; }
@@ -29,21 +33,40 @@ public:
 
     void keyPressEvent(QKeyEvent *event);
 
-    void keyReleaseEvent(QKeyEvent *event);
-
     void paintEvent(QPaintEvent *event);
 
 public slots:
     void MoveColumn();
+
     void MoveBird();
+
+    void MoveBirdBeforeStartingTheGame();
+
+    void CheckIntersections();
+
+    void SetStartingCoordinates();
+
+    void IncreaseCounter();
+
+    void DeathCase();
+
+    void StartGame();
 
 private:
     Ui::FlappyBird *ui;
     std::vector<QPoint> points;
     QPoint point_for_bird;
-    int x_column_pace = 0;
-    int bird_pace = 0;
+    int starting_bird_pace = -2;
+    int x_column_pace = -4;
+    int bird_pace = -10;
     int radius = 25;
+    int counter = 0;
+    bool death = false;
+    QTimer* column_timer = new QTimer(this);
+    QTimer* bird_timer = new QTimer(this);
+    QTimer* starting_bird_timer = new QTimer(this);
+    QString score;
+    std::vector<QString> phrases;
 };
 
 #endif // FLAPPYBIRD_H
