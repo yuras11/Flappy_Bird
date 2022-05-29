7d5b7475913cb24cb96e9d2002722147a5102498
Юрий <90134356+yuras11@users.noreply.github.com>
2022-05-29 18:25:17 +0300

    Update column.cpp

--- column.cpp
+++ column.cpp
@@ -3,14 +3,14 @@
 Column::Column(QPoint column_point)
 {
     higher_rect.setX(column_point.x()-25);
-    higher_rect.setY(0);
+    higher_rect.setY(column_point.y()-747);
     higher_rect.setWidth(70);
-    higher_rect.setHeight(column_point.y()-50);
+    higher_rect.setHeight(697);
 
     lower_rect.setX(column_point.x()-25);
     lower_rect.setY(column_point.y()+70);
     lower_rect.setWidth(70);
-    lower_rect.setHeight(700);
+    lower_rect.setHeight(547);
 }
 
 void Column::DrawColumn(QPainter *painter, QImage lower_image, QImage upper_image)
