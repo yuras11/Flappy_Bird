996b738af949cf35ca1d2cff50d502439a3e1efb
Юрий <90134356+yuras11@users.noreply.github.com>
2022-05-29 18:30:32 +0300

    Create settingswindow.h

---
+++ settingswindow.h
@@ -0,0 +1,56 @@
+#ifndef SETTINGSWINDOW_H
+#define SETTINGSWINDOW_H
+
+#include <QDialog>
+#include <QPainter>
+#include <QString>
+#include <string>
+#include <QFont>
+#include <vector>
+
+namespace Ui {
+class SettingsWindow;
+}
+
+class SettingsWindow : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit SettingsWindow(QWidget *parent = nullptr);
+
+    ~SettingsWindow();
+
+    void paintEvent(QPaintEvent *event);
+
+    void SetMainBackgroundImage(QImage image);
+
+private slots:
+    void on_ReturnButton_clicked();
+
+    void on_HeightOfJumpSlider_sliderMoved(int position);
+
+    void on_ColumnPaceSlider_sliderMoved(int position);
+
+    void on_PaceOfFallSlider_sliderMoved(int position);
+
+    void on_ButtonForBackground_clicked();
+
+    void on_ClearBackgroundButton_clicked();
+
+signals:
+    void SignalForHeightOfJump(int height_of_jump);
+
+    void SignalForColumnPace(int pace);
+
+    void SignalForBirdPaceOfFall(int pace);
+
+    void SignalForBackgroundImage(QImage image);
+
+private:
+    Ui::SettingsWindow *ui;
+    std::vector<QString> phrases;
+    QImage main_background_image;
+};
+
+#endif // SETTINGSWINDOW_H
