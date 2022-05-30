f4bd98eb59359be93181019eb219cba48e8a746a
Юрий <90134356+yuras11@users.noreply.github.com>
2022-05-30 15:46:43 +0300

    Update basement.cpp

--- basement.cpp
+++ basement.cpp
@@ -1,14 +1,18 @@
 #include "basement.h"
 
-Basement::Basement(QPoint point_for_basement)
+Basement::Basement(): image_for_basement("C:/Users/Yuriy Kozlov/Documents/flappybird/basement.png"){}
+
+void Basement::DrawBasement(QPainter* painter, QPoint point)
 {
-    rect_for_basement.setX(point_for_basement.x());
+    rect_for_basement.setX(point.x());
     rect_for_basement.setY(550);
-    rect_for_basement.setWidth(805);
+    rect_for_basement.setWidth(810);
     rect_for_basement.setHeight(100);
+
+    painter->drawImage(rect_for_basement, image_for_basement);
 }
 
-void Basement::DrawBasement(QPainter* painter, QImage image)
+void Basement::SetBasementPace(int pace)
 {
-    painter->drawImage(rect_for_basement, image);
+    basement_pace = -pace;
 }
