c083083a348d13c65aa316bce2014de49a14b66f
Юрий <90134356+yuras11@users.noreply.github.com>
2022-06-23 00:29:32 +0300

    Update bird.cpp

--- bird.cpp
+++ bird.cpp
@@ -2,38 +2,32 @@
 
 Bird::Bird(): point_for_bird(200, 300)
 {
-    QImage first_image("C:/Users/Yuriy Kozlov/Documents/flappybird/bird_pictures/yellow_bird/first.png");
-    bird_images.push_back(first_image);
-    bird_images.push_back(first_image);
-    bird_images.push_back(first_image);
-    QImage second_image("C:/Users/Yuriy Kozlov/Documents/flappybird/bird_pictures/yellow_bird/second.png");
-    bird_images.push_back(second_image);
-    bird_images.push_back(second_image);
-    bird_images.push_back(second_image);
-    QImage third_image("C:/Users/Yuriy Kozlov/Documents/flappybird/bird_pictures/yellow_bird/third.png");
-    bird_images.push_back(third_image);
-    bird_images.push_back(third_image);
-    bird_images.push_back(third_image);
-    QImage forth_image("C:/Users/Yuriy Kozlov/Documents/flappybird/bird_pictures/yellow_bird/forth.png");
-    bird_images.push_back(forth_image);
-    bird_images.push_back(forth_image);
-    bird_images.push_back(third_image);
+    QString fileName("C:/Users/Yuriy Kozlov/Documents/flappybird/bird_pictures/yellow_bird/first.png");
+    bird_images.push_back(fileName);
+    fileName = "C:/Users/Yuriy Kozlov/Documents/flappybird/bird_pictures/yellow_bird/second.png";
+    bird_images.push_back(fileName);
+    fileName = "C:/Users/Yuriy Kozlov/Documents/flappybird/bird_pictures/yellow_bird/third.png";
+    bird_images.push_back(fileName);
+    fileName = "C:/Users/Yuriy Kozlov/Documents/flappybird/bird_pictures/yellow_bird/forth.png";
+    bird_images.push_back(fileName);
 
-    main_image = bird_images[0];
+    main_image = fileName;
 }
 
 void Bird::DrawBird(QPainter *painter)
 {
+    QImage main_image(this->main_image);
     painter->drawImage(point_for_bird, main_image);
 }
 
 void Bird::BirdAnimation()
 {
-    index = (index + 1) % 12;
+    index = (index + 1) % bird_images.size();
+
     main_image = bird_images[index];
 }
 
-void Bird::SetBirdImages(std::vector<QImage> &skins)
+void Bird::SetBirdImages(std::vector<QString> &skins)
 {
     bird_images = skins;
 }
@@ -46,4 +40,19 @@ void Bird::SetBirdPaceOfFall(int pace)
 void Bird::SetHeightOfJump(int height)
 {
     height_of_jump = -height;
+}
+
+void Bird::MoveBird()
+{
+    point_for_bird.setY(point_for_bird.y() + bird_pace);
+    bird_pace += bird_pace_of_fall;
+}
+
+void Bird::MoveBirdBeforeStartingTheGame()
+{
+    point_for_bird.setY(point_for_bird.y() + starting_bird_pace);
+    if(point_for_bird.y() <= 285 || point_for_bird.y() >= 315)
+    {
+        starting_bird_pace = -starting_bird_pace;
+    }
 }
