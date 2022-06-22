8257e5a78f0c5fbb794ce3ae2459118a2e05b4f0
Юрий <90134356+yuras11@users.noreply.github.com>
2022-06-23 00:30:55 +0300

    Update column.cpp

--- column.cpp
+++ column.cpp
@@ -1,22 +1,75 @@
 #include "column.h"
 
-void Column::DrawColumn(QPainter *painter, QPoint point, QImage lower_image, QImage upper_image)
+Column::Column(): distance(-50), column_pace(-4)
 {
-     upper_column.setX(point.x()-25);
-     upper_column.setY(point.y()-upper_image.height());
-     upper_column.setHeight(upper_image.height()-50);
-     upper_column.setWidth(70);
+    std::random_device rdGen;
+    std::mt19937 gen(rdGen());
+    std::uniform_int_distribution<> dis0_15(70, 410);
+    for(int i = 0; i < 4; i++)
+    {
 
-     lower_column.setX(point.x()-25);
-     lower_column.setY(point.y()+70);
-     lower_column.setHeight(lower_image.height());
-     lower_column.setWidth(70);
+        QPoint point(2*800 + distance,dis0_15(gen));
+        points_for_columns.push_back(point);
+        distance += 300;
+    }
+    QImage lower_image("C:/Users/Yuriy Kozlov/Documents/flappybird/lower-pipe-green.png");
+    column_pictures.push_back(lower_image);
+    QImage upper_image("C:/Users/Yuriy Kozlov/Documents/flappybird/pipe-green.png");
+    column_pictures.push_back(upper_image);
 
-     painter->drawImage(upper_column, upper_image);
-     painter->drawImage(lower_column, lower_image);
+    for(int i = 0; i < points_for_columns.size(); i++)
+    {
+        upper_column.setX(points_for_columns[i].x()-25);
+        upper_column.setY(points_for_columns[i].y()-column_pictures[1].height());
+        upper_column.setHeight(column_pictures[1].height()-50);
+        upper_column.setWidth(70);
+
+        lower_column.setX(points_for_columns[i].x()-25);
+        lower_column.setY(points_for_columns[i].y()+70);
+        lower_column.setHeight(column_pictures[0].height());
+        lower_column.setWidth(70);
+    }
+}
+
+void Column::DrawColumns(QPainter *painter)
+{
+     painter->drawImage(upper_column, column_pictures[1]);
+     painter->drawImage(lower_column, column_pictures[0]);
 }
 
 void Column::SetColumnPace(int pace)
 {
     column_pace = -pace;
+}
+
+void Column::MoveColumn()
+{
+    std::random_device rdGen;
+    std::mt19937 gen1(rdGen());
+    std::uniform_int_distribution<> dis0_15(70, 410);
+    for(int i = 0; i < points_for_columns.size(); i++)
+    {
+        points_for_columns[i].setX(points_for_columns[i].x() + column_pace);
+        if(points_for_columns[i].x() + 50 <= 0)
+        {
+            points_for_columns[i].setX(800 + 400);
+            points_for_columns[i].setY(dis0_15(gen1));
+        }
+    }
+}
+
+bool Column::IntersectionHappened(Bird &bird)
+{
+    for(int i = 0; i < points_for_columns.size(); i++)
+    {
+        if(bird.point_for_bird.x()+bird.radius >= points_for_columns[i].x()-25 && bird.point_for_bird.x() <= points_for_columns[i].x()+45)
+        {
+            if(bird.point_for_bird.y()+bird.radius <= points_for_columns[i].y()-25 ||
+               bird.point_for_bird.y()+bird.radius >= points_for_columns[i].y()+70)
+            {
+               return true;
+            }
+        }
+    }
+    return false;
 }
